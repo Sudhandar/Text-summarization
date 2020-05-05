f505280105893c5bf0134248ab7bbb5209e78215
Sudhandar <sudhandar@gmail.com>
2020-05-05 22:40:02 +0530

    Decoder function

--- script.py
+++ script.py
@@ -234,3 +234,37 @@ def beam_search_decoder(data, k):
 		ordered = sorted(all_candidates, key=lambda tup:tup[1])
 		sequences = ordered[:k]
 	return sequences
+
+def sample_line():
+  # initial inputs
+  np_input = np.array([[ word2idx['<sos>'] ]])
+  h = np.zeros((1, LATENT_DIM))
+  c = np.zeros((1, LATENT_DIM))
+
+  # so we know when to quit
+  eos = word2idx['<eos>']
+
+  # store the output here
+  output_sentence = []
+
+  for _ in range(max_sequence_length):
+    o, h, c = sampling_model.predict([np_input, h, c])
+
+    # print("o.shape:", o.shape, o[0,0,:10])
+    # idx = np.argmax(o[0,0])
+    probs = o[0,0]
+    if np.argmax(probs) == 0:
+      print("wtf")
+    probs[0] = 0
+    probs /= probs.sum()
+    idx = np.random.choice(len(probs), p=probs)
+    if idx == eos:
+      break
+
+    # accuulate output
+    output_sentence.append(idx2word.get(idx, '<WTF %s>' % idx))
+
+    # make the next input into model
+    np_input[0,0] = idx
+
+  return ' '.join(output_sentence)
